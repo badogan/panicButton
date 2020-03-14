8cbdc2ffb9b08f8777afa85843361de563179421
badogan <basri.dogan.71@gmail.com>
2020-03-14 12:41:10 +0000

    minor update with useEffect code.

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -5,9 +5,9 @@ const SignInForm = props => {
   const [password, setPassword] = useState();
 
   const sendToUpdateUserSignIn = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     props.updateUserSignin({ username, password });
-    document.getElementById('form33').reset()
+    // document.getElementById('form33').reset()
   };
 
   const handlePassword = e => setPassword(e.target.value);
@@ -15,14 +15,14 @@ const SignInForm = props => {
 
 //   Not sure if this is necessary
 
-  useEffect(() => {
-    document.getElementById('form33').reset()
-  }, []);
+//   useEffect(() => {
+//     document.getElementById('form33').reset()
+//   }, []);
 
   return (
     <div className="signin-container wrapper">
       <h3>Signin form</h3>
-      <form id="form33" onSubmit={() => sendToUpdateUserSignIn()}>
+      <form id="form33">
         {/* <label>Username: </label> */}
         <input
           onChange={handleUsername}
